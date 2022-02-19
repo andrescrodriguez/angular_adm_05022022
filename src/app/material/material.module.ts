51eb9cec8fdb23b3485e614de44b88c314579ecf
Andrés <andres.c.rodriguez@outlook.com>
2022-02-19 16:56:03 -0300

    diseño de galeria - front

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -15,6 +15,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
@@ -33,7 +34,8 @@ import { MatCardModule } from '@angular/material/card';
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatMenuModule,
-    MatCardModule
+    MatCardModule,
+    MatTabsModule
   ],
   imports: [
     CommonModule
