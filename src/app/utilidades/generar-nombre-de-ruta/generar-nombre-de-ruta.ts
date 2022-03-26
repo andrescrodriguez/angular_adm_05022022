4f2d57634c0eede0f649ad5b2b3881b5d160e528
Andrés <andres.c.rodriguez@outlook.com>
2022-03-26 19:10:43 -0300

    expresion regular en nombre ruta

--- src/app/utilidades/generar-nombre-de-ruta/generar-nombre-de-ruta.ts
+++ src/app/utilidades/generar-nombre-de-ruta/generar-nombre-de-ruta.ts
@@ -1,9 +1,20 @@
 export function generarNombreDeRuta(valor: string): string {
     if(!valor) return '';
     if(valor.length === 0) return '';
-    return valor
-        .toLowerCase()
-        .trim()
-        .split(' ')
-        .join('-');
+
+        var specialChars = "!@#$^&%*()+=-[]\/{}|:<>¿?,."; 
+        for (var i = 0; i < specialChars.length; i++) 
+        { 
+            valor = valor.replace(new RegExp("\\" + specialChars[i], 'gi'), ''); 
+        }
+
+        valor = valor
+            .toLowerCase()
+            .trim()
+            .split(' ')
+            .join('-');
+
+        while(valor.includes('--')){ valor = valor.replace('--','-') }
+
+    return valor;
 }
